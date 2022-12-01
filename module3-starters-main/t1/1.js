abfa2705d502a0f7a43e461e65f8bb2f3418afc3
wondomen <112002375+wondomen@users.noreply.github.com>
2022-12-01 14:49:06 +0200

    java module-3

---
+++ module3-starters-main/t1/1.js
@@ -0,0 +1,7 @@
+'Use strict';
+
+document.getElementById('target').innerHTML = '<li>First item</li>\n' +
+    '<li>Second item</li>\n' +
+    '<li>Third item</li>';
+
+document.getElementById('target').classList.add('my-list');
